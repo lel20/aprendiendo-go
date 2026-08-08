663ece9eb43943aa1208e04d2acc06d0f30490d5
lel20 <leonardoparedes.v.r@gmail.com>
2026-08-08 18:50:08 +0200

    feat: primer programa en Go

---
+++ 01-hola-mundo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Leonardo"
+	edad := 33
+	lenguaje := "Programación"
+	fmt.Println("Nombre:",nombre)
+	fmt.Println("Edad:",edad)
+	fmt.Println("Lenguaje:",lenguaje)
+}
