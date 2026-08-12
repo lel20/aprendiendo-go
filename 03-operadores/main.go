f85e217186f2961354477012a4685eb73389b143
lel20 <leonardoparedes.v.r@gmail.com>
2026-08-12 14:34:28 +0200

    feat: practica operadores aritmeticos

---
+++ 03-operadores/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*precio :=100
+	descuento:=20
+	resultado:=precio-descuento
+	fmt.Println(resultado)*/
+
+
+	/*precio := 100
+	descuento := 20
+
+	importeDescuento := (precio*descuento)/100
+	precioFinal := precio-importeDescuento
+
+	fmt.Println(importeDescuento)
+	fmt.Println(precioFinal)*/
+
+
+	//Realizar el siguiente ejercicio:
+	/*	precio := 250
+		descuento :=  20 
+		importeDescuento := ???
+		precioFinal := ???
+		*/
+
+	precio:=250
+	descuento:=20
+	importeDescuento:=(precio*descuento)/100
+	precioFinal:=precio-importeDescuento
+	fmt.Println(importeDescuento)
+	fmt.Println(precioFinal)
+}
+
